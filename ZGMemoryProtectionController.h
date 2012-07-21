7057eed5ae9629b5cb7cb60ba1ac936a34537f18
Zorg <zorgiepoo@gmail.com>
2012-07-21 02:45:11 -0400

    Apparently I forgot to add these files.

---
+++ ZGMemoryProtectionController.h
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Bit Slicer.
+ *
+ * Bit Slicer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ 
+ * Bit Slicer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with Bit Slicer.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Created by Mayur Pawashe on 7/19/12.
+ * Copyright 2012 zgcoder. All rights reserved.
+ */
+
+#import <Foundation/Foundation.h>
+@class ZGDocument;
+
+@interface ZGMemoryProtectionController : NSObject
+{
+	IBOutlet ZGDocument *document;
+	
+	IBOutlet NSWindow *changeProtectionWindow;
+	IBOutlet NSTextField *changeProtectionAddressTextField;
+	IBOutlet NSTextField *changeProtectionSizeTextField;
+	IBOutlet NSButton *changeProtectionReadButton;
+	IBOutlet NSButton *changeProtectionWriteButton;
+	IBOutlet NSButton *changeProtectionExecuteButton;
+}
+
+- (IBAction)changeProtectionOkayButton:(id)sender;
+- (IBAction)changeProtectionCancelButton:(id)sender;
+
+- (void)changeMemoryProtectionRequest;
+
+@end
