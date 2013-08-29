10c9e50b6b7307a1b554decee5e3ca688a17c555
Zorg <zorgiepoo@gmail.com>
2013-08-28 21:01:04 -0400

    Whitespace

--- ZGPyVirtualMemory.m
+++ ZGPyVirtualMemory.m
@@ -140,12 +140,12 @@ static PyTypeObject VirtualMemoryType =
 	0, // tp_as_buffer
 	Py_TPFLAGS_DEFAULT, // tp_flags
 	"VirtualMemory objects", // tp_doc
-	0,	// tp_traverse
-	0,	// tp_clear
+	0, // tp_traverse
+	0, // tp_clear
 	0, // tp_richcompare
-	0,	// tp_weaklistoffset
-	0,	// tp_iter
-	0,	// tp_iternext
+	0, // tp_weaklistoffset
+	0, // tp_iter
+	0, // tp_iternext
 	VirtualMemory_methods, // tp_methods
 	VirtualMemory_members, // tp_members
 	0, // tp_getset
