ab33f4818b3bb398a3c777f5f97285cb6fb76eb5
Zorg <zorgiepoo@gmail.com>
2013-08-30 00:03:18 -0400

    Use foundation instead of cocoa for inclusion

--- ZGProcess.h
+++ ZGProcess.h
@@ -32,7 +32,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 #import "ZGMemoryTypes.h"
 #import <sys/sysctl.h>
 
