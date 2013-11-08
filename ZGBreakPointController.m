385afff9369cd293d36a12841f24eb6cf55fee01
Zorg <zorgiepoo@gmail.com>
2013-11-08 18:32:04 -0500

    Fixing multiple watchpoints not working. (from 29f949a)
    --HG--
    branch : stable

--- ZGBreakPointController.m
+++ ZGBreakPointController.m
@@ -85,11 +85,11 @@ extern boolean_t mach_exc_server(mach_msg_header_t *InHeadP, mach_msg_header_t *
 	debugState.uds.type.__dr7 &= ~(1 << (2*debugRegisterIndex)); \
 	debugState.uds.type.__dr7 &= ~(1 << (2*debugRegisterIndex+1)); \
 	\
-	debugState.uds.type.__dr7 &= ~(1 << (16 + 2*debugRegisterIndex)); \
-	debugState.uds.type.__dr7 &= ~(1 << (16 + 2*debugRegisterIndex+1)); \
+	debugState.uds.type.__dr7 &= ~(1 << (16 + 4*debugRegisterIndex)); \
+	debugState.uds.type.__dr7 &= ~(1 << (16 + 4*debugRegisterIndex+1)); \
 	\
-	debugState.uds.type.__dr7 &= ~(1 << (18 + 2*debugRegisterIndex)); \
-	debugState.uds.type.__dr7 &= ~(1 << (18 + 2*debugRegisterIndex+1)); \
+	debugState.uds.type.__dr7 &= ~(1 << (18 + 4*debugRegisterIndex)); \
+	debugState.uds.type.__dr7 &= ~(1 << (18 + 4*debugRegisterIndex+1)); \
 
 - (ZGMemoryAddress)removeWatchPoint:(ZGBreakPoint *)breakPoint
 {
@@ -614,9 +614,9 @@ kern_return_t catch_mach_exception_raise(mach_port_t exception_port, mach_port_t
 	debugState.uds.type.__dr7 |= (1 << (2*debugRegisterIndex)); \
 	debugState.uds.type.__dr7 &= ~(1 << (2*debugRegisterIndex+1)); \
 	\
-	debugState.uds.type.__dr7 |= (1 << (16 + 2*debugRegisterIndex)); \
-	if (watchPointType == ZGWatchPointWrite) { debugState.uds.type.__dr7 &= ~(1 << (16 + 2*debugRegisterIndex+1)); } \
-	else if (watchPointType == ZGWatchPointReadOrWrite) { debugState.uds.type.__dr7 |= (1 << (16 + 2*debugRegisterIndex+1)); } \
+	debugState.uds.type.__dr7 |= (1 << (16 + 4*debugRegisterIndex)); \
+	if (watchPointType == ZGWatchPointWrite) { debugState.uds.type.__dr7 &= ~(1 << (16 + 4*debugRegisterIndex+1)); } \
+	else if (watchPointType == ZGWatchPointReadOrWrite) { debugState.uds.type.__dr7 |= (1 << (16 + 4*debugRegisterIndex+1)); } \
 	\
 	if (watchSize == 1) { debugState.uds.type.__dr7 &= ~(1 << (18 + 2*debugRegisterIndex)); debugState.uds.type.__dr7 &= ~(1 << (18 + 2*debugRegisterIndex+1)); } \
 	else if (watchSize == 2) { debugState.uds.type.__dr7 |= (1 << (18 + 2*debugRegisterIndex)); debugState.uds.type.__dr7 &= ~(1 << (18 + 2*debugRegisterIndex+1)); } \
