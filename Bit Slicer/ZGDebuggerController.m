5c5dbab383ab9d1967c74e1a9763a870c7bb8499
Zorg <zorgiepoo@gmail.com>
2015-02-15 16:06:19 -0500

    Decrease dissassembly window size to 512

--- Bit Slicer/ZGDebuggerController.m
+++ Bit Slicer/ZGDebuggerController.m
@@ -1046,7 +1046,7 @@ enum ZGStepExecution
 	self.instructionBoundary = NSMakeRange(firstInstructionAddress, maxInstructionsSize);
 	
 	// Disassemble within a range from +- WINDOW_SIZE from selection address
-	const NSUInteger WINDOW_SIZE = 2048;
+	const NSUInteger WINDOW_SIZE = 512;
 	
 	ZGMemoryAddress lowBoundAddress = calculatedMemoryAddress - WINDOW_SIZE;
 	if (lowBoundAddress <= firstInstructionAddress)
