c7845c134529bc811e4bf1799ba09e3f99acb926
Roberto Nibali <roberto.nibali@engenius.ch>
2025-07-11 18:49:50 +0200

    Document: Add comprehensive file-level and function-level documentation to `ZGThreadStates`, including detailed descriptions of thread state management functions, architecture-specific implementation details, and usage examples.

--- Bit Slicer/ZGThreadStates.c
+++ Bit Slicer/ZGThreadStates.c
@@ -30,6 +30,65 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ * ZGThreadStates.c - Implementation of Thread State Management
+ * ===========================================================
+ *
+ * This file implements the functions declared in ZGThreadStates.h, providing
+ * cross-platform thread state management for debugging purposes.
+ *
+ * Thread State Transition Flow:
+ * ----------------------------
+ *
+ *                                 +----------------+
+ *                                 | Normal Program |
+ *                                 |   Execution    |
+ *                                 +--------+-------+
+ *                                          |
+ *                                          | (Breakpoint Hit)
+ *                                          v
+ *  +----------------+            +-------------------+
+ *  | ZGSetGeneral   |<-----------| ZGGetGeneral     |
+ *  | ThreadState()  |            | ThreadState()    |
+ *  +----------------+            +-------------------+
+ *          |                              |
+ *          |                              | (Access Registers)
+ *          |                              v
+ *          |                     +-------------------+
+ *          |                     | ZGInstruction     |
+ *          |                     | PointerFromGeneral|
+ *          |                     | ThreadState()     |
+ *          |                     +-------------------+
+ *          |                              |
+ *          |                              | (Modify Registers)
+ *          |                              v
+ *          |                     +-------------------+
+ *          |                     | ZGSetInstruction  |
+ *          |                     | PointerFromGeneral|
+ *          |                     | ThreadState()     |
+ *          |                     +-------------------+
+ *          |                              |
+ *          +------------------------------+
+ *                                         |
+ *                                         v
+ *                                 +----------------+
+ *                                 | Resume Program |
+ *                                 |   Execution    |
+ *                                 +----------------+
+ *
+ * Debug State Handling:
+ * --------------------
+ * For hardware breakpoints, the debug registers are managed through:
+ * - ZGGetDebugThreadState()
+ * - ZGSetDebugThreadState()
+ *
+ * Vector State Handling:
+ * ---------------------
+ * For SIMD operations, vector registers are managed through:
+ * - ZGGetVectorThreadState()
+ * - ZGSetVectorThreadState()
+ */
+
 #include "ZGThreadStates.h"
 
 #if TARGET_CPU_ARM64
@@ -38,6 +97,28 @@ typedef arm_neon_state64_t zg_float_state_t;
 typedef x86_float_state_t zg_float_state_t;
 #endif
 
+/**
+ * Retrieves the general purpose thread state for a given thread.
+ *
+ * This function uses the appropriate thread state flavor based on the architecture:
+ * - ARM_THREAD_STATE64 for ARM64
+ * - x86_THREAD_STATE for x86/x86_64
+ *
+ * Example:
+ * -------
+ * ```
+ * zg_thread_state_t threadState;
+ * mach_msg_type_number_t stateCount;
+ * if (ZGGetGeneralThreadState(&threadState, thread, &stateCount)) {
+ *     // Thread state retrieved successfully
+ * }
+ * ```
+ *
+ * @param threadState Pointer to store the thread state
+ * @param thread The thread to get state from
+ * @param stateCount Optional pointer to store the state count
+ * @return true if successful, false otherwise
+ */
 bool ZGGetGeneralThreadState(zg_thread_state_t *threadState, thread_act_t thread, mach_msg_type_number_t *stateCount)
 {
 #if TARGET_CPU_ARM64
@@ -47,12 +128,33 @@ bool ZGGetGeneralThreadState(zg_thread_state_t *threadState, thread_act_t thread
 	mach_msg_type_number_t localStateCount = x86_THREAD_STATE_COUNT;
 	thread_state_flavor_t flavor = x86_THREAD_STATE;
 #endif
-	
+
 	bool success = (thread_get_state(thread, flavor, (thread_state_t)threadState, &localStateCount) == KERN_SUCCESS);
 	if (stateCount != NULL) *stateCount = localStateCount;
 	return success;
 }
 
+/**
+ * Sets the general purpose thread state for a given thread.
+ *
+ * This function uses the appropriate thread state flavor based on the architecture:
+ * - ARM_THREAD_STATE64 for ARM64
+ * - x86_THREAD_STATE for x86/x86_64
+ *
+ * Example:
+ * -------
+ * ```
+ * // After modifying threadState
+ * if (ZGSetGeneralThreadState(&threadState, thread, stateCount)) {
+ *     // Thread state set successfully
+ * }
+ * ```
+ *
+ * @param threadState Pointer to the thread state to set
+ * @param thread The thread to set state for
+ * @param stateCount The state count
+ * @return true if successful, false otherwise
+ */
 bool ZGSetGeneralThreadState(zg_thread_state_t *threadState, thread_act_t thread, mach_msg_type_number_t stateCount)
 {
 #if TARGET_CPU_ARM64
@@ -60,10 +162,28 @@ bool ZGSetGeneralThreadState(zg_thread_state_t *threadState, thread_act_t thread
 #else
 	thread_state_flavor_t flavor = x86_THREAD_STATE;
 #endif
-	
+
 	return (thread_set_state(thread, flavor, (thread_state_t)threadState, stateCount) == KERN_SUCCESS);
 }
 
+/**
+ * Gets the instruction pointer value from a thread state.
+ *
+ * This function extracts the program counter/instruction pointer from the thread state:
+ * - pc register for ARM64
+ * - rip register for x86_64
+ * - eip register for x86
+ *
+ * Example:
+ * -------
+ * ```
+ * ZGMemoryAddress currentAddress = ZGInstructionPointerFromGeneralThreadState(&threadState, processType);
+ * ```
+ *
+ * @param threadState Pointer to the thread state
+ * @param type The process type (32-bit or 64-bit)
+ * @return The instruction pointer address
+ */
 ZGMemoryAddress ZGInstructionPointerFromGeneralThreadState(zg_thread_state_t *threadState, ZGProcessType type)
 {
 #if TARGET_CPU_ARM64
@@ -75,6 +195,26 @@ ZGMemoryAddress ZGInstructionPointerFromGeneralThreadState(zg_thread_state_t *th
 	return instructionPointer;
 }
 
+/**
+ * Sets the instruction pointer value in a thread state.
+ *
+ * This function modifies the program counter/instruction pointer in the thread state:
+ * - pc register for ARM64
+ * - rip register for x86_64
+ * - eip register for x86
+ *
+ * Example:
+ * -------
+ * ```
+ * // Skip the current instruction
+ * ZGMemoryAddress currentAddress = ZGInstructionPointerFromGeneralThreadState(&threadState, processType);
+ * ZGSetInstructionPointerFromGeneralThreadState(&threadState, currentAddress + instructionSize, processType);
+ * ```
+ *
+ * @param threadState Pointer to the thread state
+ * @param instructionAddress The new instruction pointer address
+ * @param type The process type (32-bit or 64-bit)
+ */
 void ZGSetInstructionPointerFromGeneralThreadState(zg_thread_state_t *threadState, ZGMemoryAddress instructionAddress, ZGProcessType type)
 {
 #if TARGET_CPU_ARM64
@@ -92,6 +232,26 @@ void ZGSetInstructionPointerFromGeneralThreadState(zg_thread_state_t *threadStat
 #endif
 }
 
+/**
+ * Gets the base pointer value from a thread state.
+ *
+ * The base pointer (frame pointer) is used for stack frame traversal and backtrace generation.
+ * This function extracts:
+ * - fp register for ARM64
+ * - rbp register for x86_64
+ * - ebp register for x86
+ *
+ * Example:
+ * -------
+ * ```
+ * // Get base pointer for backtrace
+ * ZGMemoryAddress basePointer = ZGBasePointerFromGeneralThreadState(&threadState, processType);
+ * ```
+ *
+ * @param threadState Pointer to the thread state
+ * @param type The process type (32-bit or 64-bit)
+ * @return The base pointer address
+ */
 ZGMemoryAddress ZGBasePointerFromGeneralThreadState(zg_thread_state_t *threadState, ZGProcessType type)
 {
 #if TARGET_CPU_ARM64
@@ -104,6 +264,28 @@ ZGMemoryAddress ZGBasePointerFromGeneralThreadState(zg_thread_state_t *threadSta
 #endif
 }
 
+/**
+ * Retrieves the debug thread state for a given thread.
+ *
+ * The debug state contains hardware breakpoint registers:
+ * - ARM_DEBUG_STATE64 for ARM64
+ * - x86_DEBUG_STATE for x86/x86_64
+ *
+ * Example:
+ * -------
+ * ```
+ * zg_debug_state_t debugState;
+ * mach_msg_type_number_t debugStateCount;
+ * if (ZGGetDebugThreadState(&debugState, thread, &debugStateCount)) {
+ *     // Debug state retrieved successfully
+ * }
+ * ```
+ *
+ * @param debugState Pointer to store the debug state
+ * @param thread The thread to get debug state from
+ * @param stateCount Optional pointer to store the state count
+ * @return true if successful, false otherwise
+ */
 bool ZGGetDebugThreadState(zg_debug_state_t *debugState, thread_act_t thread, mach_msg_type_number_t *stateCount)
 {
 #if TARGET_CPU_ARM64
@@ -113,12 +295,33 @@ bool ZGGetDebugThreadState(zg_debug_state_t *debugState, thread_act_t thread, ma
 	thread_state_flavor_t flavor = x86_DEBUG_STATE;
 	mach_msg_type_number_t localStateCount = x86_DEBUG_STATE_COUNT;
 #endif
-	
+
 	bool success = (thread_get_state(thread, flavor, (thread_state_t)debugState, &localStateCount) == KERN_SUCCESS);
 	if (stateCount != NULL) *stateCount = localStateCount;
 	return success;
 }
 
+/**
+ * Sets the debug thread state for a given thread.
+ *
+ * This function is used to configure hardware breakpoints by setting debug registers:
+ * - ARM_DEBUG_STATE64 for ARM64
+ * - x86_DEBUG_STATE for x86/x86_64
+ *
+ * Example:
+ * -------
+ * ```
+ * // After configuring debug registers in debugState
+ * if (ZGSetDebugThreadState(&debugState, thread, debugStateCount)) {
+ *     // Debug state set successfully
+ * }
+ * ```
+ *
+ * @param debugState Pointer to the debug state to set
+ * @param thread The thread to set debug state for
+ * @param stateCount The state count
+ * @return true if successful, false otherwise
+ */
 bool ZGSetDebugThreadState(zg_debug_state_t *debugState, thread_act_t thread, mach_msg_type_number_t stateCount)
 {
 #if TARGET_CPU_ARM64
@@ -126,7 +329,7 @@ bool ZGSetDebugThreadState(zg_debug_state_t *debugState, thread_act_t thread, ma
 #else
 	thread_state_flavor_t flavor = x86_DEBUG_STATE;
 #endif
-	
+
 	return (thread_set_state(thread, flavor, (thread_state_t)debugState, stateCount) == KERN_SUCCESS);
 }
 
@@ -140,9 +343,9 @@ static bool ZGGetAVXThreadState(zg_vector_state_t * avxState, thread_act_t threa
 	bool isX86_64 = ZG_PROCESS_TYPE_IS_X86_64(type);
 	mach_msg_type_number_t localStateCount = isX86_64 ? x86_AVX_STATE64_COUNT : x86_AVX_STATE32_COUNT;
 	bool success = (thread_get_state(thread, isX86_64 ? x86_AVX_STATE64 : x86_AVX_STATE32, isX86_64 ? (thread_state_t)&(avxState->ufs.as64) : (thread_state_t)&(avxState->ufs.as32), &localStateCount) == KERN_SUCCESS);
-	
+
 	if (stateCount != NULL) *stateCount = localStateCount;
-	
+
 	return success;
 }
 
@@ -159,9 +362,9 @@ static bool ZGGetFloatThreadState(zg_float_state_t *floatState, thread_act_t thr
 	bool isX86_64 = ZG_PROCESS_TYPE_IS_X86_64(type);
 	mach_msg_type_number_t localStateCount = isX86_64 ? x86_FLOAT_STATE64_COUNT : x86_FLOAT_STATE32_COUNT;
 	bool success = (thread_get_state(thread, isX86_64 ? x86_FLOAT_STATE64 : x86_FLOAT_STATE32, isX86_64 ? (thread_state_t)&(floatState->ufs.fs64) : (thread_state_t)&(floatState->ufs.fs32), &localStateCount) == KERN_SUCCESS);
-	
+
 	if (stateCount != NULL) *stateCount = localStateCount;
-	
+
 	return success;
 }
 
@@ -173,17 +376,43 @@ static bool ZGSetFloatThreadState(zg_float_state_t *floatState, thread_act_t thr
 
 #endif
 
+/**
+ * Retrieves the vector thread state for a given thread.
+ *
+ * This function handles SIMD/vector registers:
+ * - ARM_NEON_STATE64 for ARM64 (NEON registers)
+ * - x86_AVX_STATE for x86/x86_64 (AVX registers if supported)
+ * - Falls back to x86_FLOAT_STATE for x86/x86_64 if AVX is not supported
+ *
+ * Example:
+ * -------
+ * ```
+ * zg_vector_state_t vectorState;
+ * bool hasAVXSupport = false;
+ * if (ZGGetVectorThreadState(&vectorState, thread, NULL, processType, &hasAVXSupport)) {
+ *     // Vector state retrieved successfully
+ *     // hasAVXSupport indicates whether AVX registers are available
+ * }
+ * ```
+ *
+ * @param vectorState Pointer to store the vector state
+ * @param thread The thread to get vector state from
+ * @param stateCount Optional pointer to store the state count
+ * @param type The process type (32-bit or 64-bit)
+ * @param hasAVXSupport Optional pointer to store whether AVX is supported
+ * @return true if successful, false otherwise
+ */
 bool ZGGetVectorThreadState(zg_vector_state_t *vectorState, thread_act_t thread, mach_msg_type_number_t *stateCount, ZGProcessType type, bool *hasAVXSupport)
 {
 #if TARGET_CPU_ARM64
 	(void)type;
-	
+
 	mach_msg_type_number_t localStateCount = ARM_NEON_STATE64_COUNT;
 	bool success = (thread_get_state(thread, ARM_NEON_STATE64, (thread_state_t)vectorState, &localStateCount) == KERN_SUCCESS);
-	
+
 	if (hasAVXSupport != NULL) *hasAVXSupport = false;
 	if (stateCount != NULL) *stateCount = localStateCount;
-	
+
 	return success;
 #else
 	if (ZGGetAVXThreadState((zg_vector_state_t *)vectorState, thread, stateCount, type))
@@ -191,27 +420,48 @@ bool ZGGetVectorThreadState(zg_vector_state_t *vectorState, thread_act_t thread,
 		if (hasAVXSupport != NULL) *hasAVXSupport = true;
 		return true;
 	}
-	
+
 	if (hasAVXSupport != NULL) *hasAVXSupport = false;
-	
+
 	return ZGGetFloatThreadState((zg_float_state_t *)vectorState, thread, stateCount, type);
 #endif
 }
 
+/**
+ * Sets the vector thread state for a given thread.
+ *
+ * This function handles SIMD/vector registers:
+ * - ARM_NEON_STATE64 for ARM64 (NEON registers)
+ * - x86_AVX_STATE for x86/x86_64 (AVX registers if supported)
+ * - Falls back to x86_FLOAT_STATE for x86/x86_64 if AVX is not supported
+ *
+ * Example:
+ * -------
+ * ```
+ * // After modifying vector registers in vectorState
+ * if (ZGSetVectorThreadState(&vectorState, thread, stateCount, processType)) {
+ *     // Vector state set successfully
+ * }
+ * ```
+ *
+ * @param vectorState Pointer to the vector state to set
+ * @param thread The thread to set vector state for
+ * @param stateCount The state count
+ * @param type The process type (32-bit or 64-bit)
+ * @return true if successful, false otherwise
+ */
 bool ZGSetVectorThreadState(zg_vector_state_t *vectorState, thread_act_t thread, mach_msg_type_number_t stateCount, ZGProcessType type)
 {
 #if TARGET_CPU_ARM64
 	(void)type;
-	
+
 	return (thread_set_state(thread, ARM_NEON_STATE64, (thread_state_t)vectorState, stateCount) == KERN_SUCCESS);
 #else
 	if (ZGSetAVXThreadState((zg_vector_state_t *)vectorState, thread, stateCount, type))
 	{
 		return true;
 	}
-	
+
 	return ZGSetFloatThreadState((zg_float_state_t *)vectorState, thread, stateCount, type);
 #endif
 }
-
-
